5825c492d7d2475c8da848a8fa5749813e3be822
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2024-05-13 23:36:53 +0000

    Python 1.0 / Guanabara

--- Guanabara.py/Atividades Guanabara 6.0.py
+++ Guanabara.py/Atividades Guanabara 6.0.py
@@ -3,7 +3,13 @@ import os
 
 os.system("cls || clear")
 
-n = int(input("Digite algo:"))
+n = input("Digite algo:") 
 
-print("||Informando o tipo||")
-print(type(n))
+print("Tipo da variável", type(n))
+print("Só tem espaços?", n.isspace())#Aceita apenas espaços 
+print("É número?", n.isnumeric())#Aceita apenas números 
+print("É alfabético?", n.isalpha())#Aceita apenas letras 
+print("É alfanúmerico?", n.isalnum())#Aceita letras e números 
+print("Está em maiúscula?", n.isupper())#Aceita apenas letras MAIÚSCULOS 
+print("Está em minúscula?", n.islower())#Aceita apenas letras minúsculas
+print("Está capitalizada?", n.istitle())#Aceita apenas letras nesse formato "Python"
