8d16f64fc50437dfe2ae4695aa0f52a8a3cf3bee
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2024-05-18 02:22:57 +0000

    Python 1.0 / Guanabara

---
+++ Guanabara.py/Atividades Guanabara 10.py
@@ -0,0 +1,10 @@
+import os 
+
+os.system("cls || clear")
+
+m = float(input("Digite quantos metros possue o objeto:"))
+
+cm = m * 100
+mm = m * 1000
+
+print(f"Valor em metros = {m}\nConvertido em cm = {cm}\nConvertido em mm = {m} ")
--- Guanabara.py/Tipo booleano.py
+++ Guanabara.py/Tipo booleano.py
@@ -4,6 +4,6 @@ os.system("cls || clear")
 
 n = input("Digite um número:")
 
-#print(n.isnumeric())
-#print(n.isalpha())
+print(n.isnumeric())
+print(n.isalpha())
 print(n.isalnum())
