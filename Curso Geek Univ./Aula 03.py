5d5990b57968cb7d3f628dc62ed6d33c7c17ddee
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2024-07-31 01:50:49 +0000

    Geek Univ.

--- Curso Geek Univ./Aula 03.py
+++ Curso Geek Univ./Aula 03.py
@@ -1,28 +1,69 @@
 """
 Recebendo dados do usuário
+
+input()  -> Todo dado recebido via input é do tipo String
+
+Em Python, string é tudo que estiver entre: 
+-Aspas simples 
+-Aspas duplas 
+-Aspas simples triplas 
+-Aspas duplas triplas 
+
+Exemplos:
+-Aspas simples =  'Angelina Jolie'
+-Aspas duplas = "angelina"
+-Aspas simples triplas = '''Angelina Jolie'''
 """
-#Entrada de dados 
-print("Qual é o seu nome?")
-nome = input()# Input -> Entrada 
+#-Aspas duplas triplas = """ Angelina Jolie """ 
 
-# Exemplo de print "antigo" 2.x
-# print("Seja bem-vindo(a) %s" % nome)
 
-# Exemmplo de print "moderno" 3.x
+
+# Entrada de dados 
+# print("Qual é o seu nome?")
+# nome = input()# Input -> Entrada 
+
+nome = input("Qual é o seu nome?")
+
+# Exemplo de print 'antigo' 2.x
+# print("Seja bem vindo(a) %s" % nome)
+
+
+
+# Exemplo de print 'moderno' 3.x
 # print("Seja bem-vindo(a) {0}". format(nome))
 
-# Exemplo de print "atual" 3.7
-print(f"Seja bem vindo(a) {nome}")
 
 
-print("Qual é a sua nota?")
-idade = input()
+# Exemplo de print 'mais atual' 3.7
+print(f"Seja bem-vindo(a) {nome}")
+
+
+# print("Qual a sua idade?")
+# idade = input()
+
+# idade = input("Qual é a sua idade?")
+idade = int(input("Qual é a sua idade?"))
+
 
 #Processamento
 
-#Saída 
-# Exemplo de print "antigo" 2.x
-# print("A %stem %s anos" % (nome, idade)) 
 
-#Exemmplo de print "moderno" 3.x
+# Saída 
+# Exemplo de print 'antigo' 2.x
+# print("A %s tem %s anos" % (nome, idade))
+
+# Exemplo de print 'moderno' 3.x
 # print("A {0} tem {1} anos".format(nome, idade))
+
+# Exemplo de print 'mais atual' 3.7
+print(f"A {nome}tem {idade} anos")
+
+"""
+int(idade) => cast
+
+Cast é a 'conversão' de um tipo de dado para outro.
+"""
+
+# print(f"A {nome} nasceu em {2018 - int(idade)}")
+
+print(f"A {nome} nasceu em {2018 - idade}")
